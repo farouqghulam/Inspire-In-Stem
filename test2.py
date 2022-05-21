a825b106da5ce707767a1b395f70d152f8bf7591
farouqghulam <farouqgmj@gmail.com>
2022-05-21 02:47:15 -0700

    did some few tests

---
+++ test2.py
@@ -0,0 +1,59 @@
+#print("five is greater than two")
+
+#if 5>2:
+  #  print("five is greater than two!")
+
+#X = str(4)
+#Y = int(4)
+#Z = float(4)
+
+#X, Y, Z ="orange", "banana", "cherry"
+#X=Y=Z ="orange"
+
+#fruits = ["apple", "banana", "cherry"]
+#X,Y,Z = fruits
+#print((X))
+#print((Y))
+#print((Z))
+
+#X = "come we code together"
+#print(X)
+
+#X ="come "
+#Y ="we "
+#Z ="code "
+#print(X,Y,Z)
+
+#x=90
+#y=10
+
+
+#x=90
+#y="johanesburg"
+#print(x+y)
+
+#x = "super B"
+#def myfunc():
+    #x = "fantastic"
+    #print("isnt it", + x)
+
+    #myfunc()
+    #print("isnt it " + x)
+
+#import random
+#print(random.randrange(1, 100))
+
+#a = "hello there"
+#print(a[0])
+
+#for x in "caroluslinus":
+    #print(x)
+
+#a = "hello world"
+#print(len(a))
+
+#txt = "the best things in life are free"
+#print("yes")
+
+txt = "the best thing is to graduate in a university"
+print("expensive" not in txt)
